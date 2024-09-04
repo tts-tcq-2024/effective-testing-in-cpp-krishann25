572fd8b94131e98041bfbecf401f25092d6ce32f
krishann25 <89766987+krishann25@users.noreply.github.com>
2024-09-04 13:23:22 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,22 +1,35 @@
+
 #include <iostream>
-#include <assert.h>
+#include <cassert>
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
+    if (cms < 38) {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if (cms >= 38 && cms <= 42) {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else if (cms > 42) {
         sizeName = 'L';
     }
     return sizeName;
 }
 
-int main() {
+void testSizeFunction() {
     assert(size(37) == 'S');
+    assert(size(38) == 'M');
     assert(size(40) == 'M');
+    assert(size(42) == 'M');
     assert(size(43) == 'L');
+    
+    assert(size(38) == 'M');
+    assert(size(42) == 'M');
+
+    assert(size(-1) == '\0');
+    assert(size(0) == 'S');
+}
+
+int main() {
+    testSizeFunction();
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
